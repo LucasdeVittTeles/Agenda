7c4e46f7b0fd5605b5b7f70ace33e951f69dd248
Lucas <lucasdevittteles@gmail.com>
2026-09-06 16:31:01 -0300

    salvando base do Identity

--- Agenda.Identity/Program.cs
+++ Agenda.Identity/Program.cs
@@ -1,6 +1,15 @@
+using Agenda.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddIdentityServer()
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddInMemoryClients(Config.Clients);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseIdentityServer();
 
 app.Run();
